58e091edc1903c129bc0d00c783b9a085dc75dd2
Matheus Gonçalves RIbeiro <matheus.gr@aluno.ufop.edu.br>
2021-04-20 16:41:37 -0300

    master

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,9 @@
 import express, { request, response } from "express";
 
+import "./database";
+import { routes } from "./routes";
+
 const app = express();
-app.get("/", (request, response) => {
-  return response.json({
-    message: "Hello World!",
-  });
-});
-
-app.post("/users", (request, response) => {
-  return response.json({ message: "Saved!" });
-});
-
-app.listen(3333, () => console.log("Server is running on port 33333"));
+app.use(express.json()); 
+app.use(routes);
+app.listen(3333, () => console.log("Server is running on port 3333"));
